c741de0b681cfd5b00b88e7d7b5a7d7d2e12fa3b
sarunaskvietka <sarunas.kvietka@gmail.com>
2018-01-30 14:37:06 +0200

    laikina

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("iveskite savo varda");
+        Scanner sc = new Scanner(System.in);
+        String vardas = sc.nextLine();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+
 
     }
 }
